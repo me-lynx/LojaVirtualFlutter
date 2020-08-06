95349affaf81d8f9aec1e79163805061d96d192f
Maiza Louise <maiza.louise@gamasti.com.br>
2020-08-05 21:12:20 -0300

    Update

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -5,24 +5,43 @@ import 'package:loja_virtual_pro/helpers/firebase_errors.dart';
 import 'package:loja_virtual_pro/models/user.dart';
 
 class UserManager extends ChangeNotifier {
-  final FirebaseAuth auth = FirebaseAuth.instance;
-  bool loading = false;
+  UserManager() {
+    _loadCurrentUser();
+  }
 
-  Future<void> signIn({User user, Function onFail, Function onSucess}) async {
-    setLoading(true);
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  FirebaseUser user;
+
+  bool _loading = false;
+  bool get loading => _loading;
+
+  Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
+    loading = true;
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
 
-      print(result.user.uid);
+      this.user = result.user;
+
+      onSuccess();
     } on PlatformException catch (e) {
-      onFail((getErrorString(e.code)));
+      onFail(getErrorString(e.code));
     }
-    setLoading(false);
+    loading = false;
   }
 
-  void setLoading(bool value) {
-    loading = value;
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  Future<void> _loadCurrentUser() async {
+    final FirebaseUser currentUser = await auth.currentUser();
+    if (currentUser != null) {
+      user = currentUser;
+      print(user.uid);
+    }
     notifyListeners();
   }
 }
